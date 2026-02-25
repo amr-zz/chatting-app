3dc6e909534778cab97ed834b5eadf54e4fbd97b
Amr Mohamed <amrmohm629@gmail.com>
2026-02-25 09:03:41 +0200

    (feat): create two endpoints for user registration and user listing

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .serializers import UserRegistrationSerializer, UserSerializer
+from rest_framework import status
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from .models import MyUser
+from rest_framework.views import APIView
 
 # Create your views here.
+
+class UserRegsterationView(APIView):
+    def post(self,request,*args,**kwargs):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class ListUsers(APIView):
+
+    permission_classes = [IsAdminUser]
+
+    def get(self, request, format=None):
+        return Response(UserSerializer(MyUser.objects.all(),many=True).data)
