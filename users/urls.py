a0cd2229e41b676b80db640c601163dcd8d66dd6
Amr Mohamed <amrmohm629@gmail.com>
2026-02-26 01:03:03 +0200

    (feat): configure the url for the register page

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegsterationView, ListUsers
+from .views import UserRegsterationView, ListUsers, register_page
 
 urlpatterns =[
+    path('register-page/', register_page),
     path('register/', UserRegsterationView.as_view(), name='register'),
     path('', ListUsers.as_view())
 ]
