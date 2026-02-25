fe3dce27582e4734c7d3203166421db98d53cf06
Amr Mohamed <amrmohm629@gmail.com>
2026-02-26 01:40:27 +0200

    (feat): gain access and refresh tokens upon register

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .serializers import UserRegistrationSerializer, UserSerializer
 from rest_framework import status
 from rest_framework.permissions import IsAdminUser
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from .models import MyUser
 from rest_framework.views import APIView
@@ -12,8 +13,10 @@ class UserRegsterationView(APIView):
     def post(self,request,*args,**kwargs):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({"refresh": str(refresh),
+                             'access': str(refresh.access_token)}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 def register_page(request):
