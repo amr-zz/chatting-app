414c2f44f07f15129a3d07af12872ea1ac5cd40c
Amr Mohamed <amrmohm629@gmail.com>
2026-02-25 07:45:13 +0200

    (feat): add new fields to the user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class MyUser(AbstractUser):
+    profile_image = models.ImageField(
+        upload_to='profile_images/',
+        blank=True,
+        null=True,
+        default='profile_images/default.jpg'
+    )
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS  = ['email',]
+
+
