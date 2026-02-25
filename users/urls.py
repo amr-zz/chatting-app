b9a9196e710d5f9b4fe0d6a2bbc4152757a55a39
Amr Mohamed <amrmohm629@gmail.com>
2026-02-26 01:40:48 +0200

    (feat): configure urls for the login

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from .views import UserRegsterationView, ListUsers, register_page
 
 urlpatterns =[
     path('register-page/', register_page),
     path('register/', UserRegsterationView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', ListUsers.as_view())
 ]
