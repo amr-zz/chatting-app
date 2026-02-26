9f042aa706b022489bc402d02ac3e8f1a2f4d09a
Amr Mohamed <amrmohm629@gmail.com>
2026-02-26 09:20:58 +0200

    (feat): create the update conversation endpoint to enable the owner to edit conversation details

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+import json
 from .serializers import ConversationSerializer, MessageSerializer
 from rest_framework import status
 from .models import Conversation
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
 from rest_framework.response import Response
 
 # Create your views here.
@@ -22,7 +24,9 @@ class CreateConversation(APIView):
     
 
 class ListConversations(APIView):
+
     permission_classes = [IsAuthenticated]
+
     def get(self, request, format=None):
         return Response(ConversationSerializer(Conversation.objects.filter(members=request.user),many=True).data)
     
@@ -31,7 +35,6 @@ class DetailConversation(APIView):
 
     def get(self, request, pk=None, format=None):
         qs = Conversation.objects.filter(pk=pk, members=request.user)
-
         if not qs.exists():
             return Response({"error": f"Conversation with id {pk} not found or you are not a member."},status=status.HTTP_404_NOT_FOUND)
         serializer = ConversationSerializer(qs[0])
@@ -43,8 +46,25 @@ class DisplayConversationMessages(APIView):
     def get(self,request,pk=None, format=None):
 
         qs = Conversation.objects.filter(pk=pk, members=request.user)
-        print(qs)
         if not qs.exists():
             return Response({"error": f"Conversation with id {pk} not found or you are not a member."},status=status.HTTP_404_NOT_FOUND)
         serializer = ConversationSerializer(qs[0])
         return Response(serializer.data['messages'])
+
+class UpdateConversation(APIView):
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def patch(self, request, pk=None, format=None):
+        
+        try:
+            conversation = Conversation.objects.get(pk=pk, members=request.user)
+        except Conversation.DoesNotExist:
+            return Response({"message": f"Conversation with id {pk} not found or you are not a member."}, status=status.HTTP_404_NOT_FOUND)
+        self.check_object_permissions(request, conversation)
+        serializer = ConversationSerializer(conversation, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Conversation updated successfully!"})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
