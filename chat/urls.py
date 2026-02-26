5666c44a9a6fde27651606baae4cf9be515e7da3
Amr Mohamed <amrmohm629@gmail.com>
2026-02-26 08:35:17 +0200

    (feat): Configure urls for the detail conversation view and display conversation messages view

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CreateConversation,ListConversations
+from .views import CreateConversation,ListConversations, DetailConversation, DisplayConversationMessages
 
 
 urlpatterns = [
     path('', ListConversations.as_view(), name='list-convos'),
+    path('<int:pk>/', DetailConversation.as_view()),
+    path('messages/<int:pk>/',DisplayConversationMessages.as_view()),
     path('create/', CreateConversation.as_view())
 ]
