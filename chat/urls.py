188ff546bc031d51262d9f313687639dff3c702f
Amr Mohamed <amrmohm629@gmail.com>
2026-02-26 09:30:09 +0200

    (feat): handle url configuration for the delete endpoint

--- chat/urls.py
+++ chat/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from .views import CreateConversation,ListConversations, DetailConversation, DisplayConversationMessages, UpdateConversation
+from .views import (CreateConversation,
+                    ListConversations,
+                    DetailConversation,
+                    DisplayConversationMessages,
+                    UpdateConversation,
+                    DeleteConversation)
 
 
 urlpatterns = [
     path('', ListConversations.as_view(), name='list-convos'),
     path('<int:pk>/', DetailConversation.as_view()),
     path('update/<int:pk>/', UpdateConversation.as_view()),
+    path('delete/<int:pk>/', DeleteConversation.as_view()),
     path('messages/<int:pk>/',DisplayConversationMessages.as_view()),
     path('create/', CreateConversation.as_view())
 ]
