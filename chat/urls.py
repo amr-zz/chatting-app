0b8e605bc66602e714242d11696dca88f79e9a86
Amr Mohamed <amrmohm629@gmail.com>
2026-02-26 09:21:23 +0200

    (feat): handle url configuration for the update conversation endpoint

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CreateConversation,ListConversations, DetailConversation, DisplayConversationMessages
+from .views import CreateConversation,ListConversations, DetailConversation, DisplayConversationMessages, UpdateConversation
 
 
 urlpatterns = [
     path('', ListConversations.as_view(), name='list-convos'),
     path('<int:pk>/', DetailConversation.as_view()),
+    path('update/<int:pk>/', UpdateConversation.as_view()),
     path('messages/<int:pk>/',DisplayConversationMessages.as_view()),
     path('create/', CreateConversation.as_view())
 ]
