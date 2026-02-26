423a7c8911dc16e3bb86e4bad387e3bd91e59cb9
Amr Mohamed <amrmohm629@gmail.com>
2026-02-26 07:49:55 +0200

    (fix): make the user who sent the request to create the conversation the conversation leader

--- chat/serializers.py
+++ chat/serializers.py
@@ -22,20 +22,20 @@ class ConversationSerializer(serializers.ModelSerializer):
             'messages',
             'member_count'
         ]
-        read_only_fields = ['messages']
+        read_only_fields = ['messages','conversation_created_by']
 
     def get_member_count(self,obj):
         return obj.members.count()
     
     def create(self,validated_data):
         members_data = validated_data.pop('members',[])
-        creator = validated_data['conversation_created_by']
-        if creator not in members_data:
-            members_data.append(creator)
+        user = self.context['request'].user
+        if user not in members_data:
+            members_data.append(user)
         conversation = Conversation.objects.create(
             conversation_name=validated_data['conversation_name'],
             conversation_description=validated_data['conversation_description'],
-            conversation_created_by=validated_data['conversation_created_by'],
+            conversation_created_by=user,
         )
         conversation.members.set(members_data)
         conversation.messages.set([])
