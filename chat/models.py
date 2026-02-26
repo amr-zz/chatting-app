4c055b1db2170ad0ac3d02e6466f85dece3a27b8
Amr Mohamed <amrmohm629@gmail.com>
2026-02-26 07:25:38 +0200

    (fix): make sent_at and created_at an auto_now_add fields and change the relationship for the message from ManyToMany to OneToMany

--- chat/models.py
+++ chat/models.py
@@ -5,14 +5,17 @@ from datetime import datetime
 
 
 
-class Message(models.Model):
-    message_content = models.CharField(max_length=1024)
-    message_sender = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    message_sent_at = datetime.now().strftime('%Y/%m/%d %H:%M')
+
 
 class Conversation(models.Model):
     conversation_name = models.CharField(max_length=256)
     conversation_description = models.TextField()
     conversation_created_by = models.ForeignKey(MyUser, on_delete=models.DO_NOTHING,related_name='created_by')
     members = models.ManyToManyField(MyUser,related_name='members',null=False)
-    messages = models.ManyToManyField(Message, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE,related_name='messages')
+    message_content = models.CharField(max_length=1024)
+    message_sender = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    message_sent_at = models.DateTimeField(auto_now_add=True)
