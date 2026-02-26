edcaec0f74cb28074c29fa1341f205a7bfd8c458
Amr Mohamed <amrmohm629@gmail.com>
2026-02-26 02:05:42 +0200

    (feat): configure the url for the logout

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 
 from .views import UserRegsterationView, ListUsers, register_page
 
@@ -8,5 +8,6 @@ urlpatterns =[
     path('register/', UserRegsterationView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('', ListUsers.as_view())
 ]
