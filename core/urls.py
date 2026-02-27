38f57efd2151d6a7a8f67dd914a6b4d5dd0abf10
Amr Mohamed <amrmohm629@gmail.com>
2026-02-27 08:26:08 +0200

    (feat): configure endpoints for the frontend

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/',include('users.urls')),
     path('api/auth/',include('authemail.urls')),
-    path('api/chat/',include('chat.urls'))
+    path('api/chat/',include('chat.urls')),
+    path('', include('frontend.urls'))
 ]
