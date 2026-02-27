5d2b879cb4a19a56867097821555428ff1b06d9f
Amr Mohamed <amrmohm629@gmail.com>
2026-02-27 06:39:42 +0200

    (feat): add auth endpoints to reset password

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/',include('users.urls')),
+    path('api/auth/',include('authemail.urls')),
     path('api/chat/',include('chat.urls'))
 ]
