a4dc84e2f8c6694debac7561f877aabc2e423f57
Amr Mohamed <amrmohm629@gmail.com>
2026-02-27 10:07:22 +0200

    (feat): use the conversation list and conversation create api endpoints

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index, reset_password, register_page, login_page, forget_password
+from .views import index, reset_password, register_page, login_page, forget_password, conversations
 urlpatterns = [
     path('', index, name='index'),
     path('reset-password/', reset_password, name='password_reset'),
     path('forget-password/', forget_password, name='forget_password'),
     path('register/', register_page, name='register'),
     path('login/', login_page, name='login'),
+    path('conversations/', conversations, name='conversations'),
+    path('conversations/create/', conversations, name='create_conversation'),
 ]
