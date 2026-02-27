73eecbca7a29eddbc2c344768b05f5b2d4bf895f
Amr Mohamed <amrmohm629@gmail.com>
2026-02-27 06:39:16 +0200

    (fix): configure the user admin to meet the authemail needs

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from users.models import MyUser
 from chat.models import Conversation, Message
+from django.contrib.auth import get_user_model
+from authemail.admin import EmailUserAdmin
 
 # Register your models here.
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(EmailUserAdmin):
     list_display = ('email', 'username', 'is_staff', 'first_name', 'last_name')
     search_fields = ('email', 'username', 'first_name', 'last_name')
     list_filter = ('is_staff', 'is_active')
@@ -17,6 +19,7 @@ class MessageAdmin(admin.ModelAdmin):
     list_display = ('conversation__conversation_name', 'message_content', 'message_sender', 'message_sent_at')
     search_fields = ('conversation__conversation_name', 'message_content', 'message_sender__username')
 
-admin.site.register(MyUser, UserAdmin)
+admin.site.unregister(get_user_model())
+admin.site.register(get_user_model(), UserAdmin)
 admin.site.register(Conversation, ConversationAdmin)
 admin.site.register(Message, MessageAdmin)
