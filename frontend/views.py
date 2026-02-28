dc57f57592d78690c6a90650dfb57031c228435c
Amr Mohamed <amrmohm629@gmail.com>
2026-02-28 06:56:03 +0200

    (feat): handle the logic for the create , edit, discover conversations and the conversation chat endpoints

--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+from chat.models import Conversation
+import json
+from chat.serializers import ConversationSerializer
 
 # Create your views here.
 
@@ -22,4 +25,21 @@ def conversations(request):
     return render(request, 'frontend/conversations.html')
 
 def create_conversation(request):
-    return render(request, 'frontend/create_edit_group.html')
+    return render(request, 'frontend/create_edit_group.html', {'is_edit': False, 'group': None})
+
+def edit_conversation(request, pk=None):
+    conversation = ConversationSerializer(Conversation.objects.filter(pk=pk).first())
+    if conversation:
+        return render(request, 'frontend/create_edit_group.html', {'is_edit': True, 'group':conversation.data})
+    else:
+        return redirect('/conversations/create/')
+    
+def discover_conversations(request):
+    return render(request, 'frontend/discover_groups.html')
+
+
+def conversation_chat(request, conversation_id=None):
+    conversation = get_object_or_404(Conversation, id=conversation_id)
+    return render(request, 'frontend/chatting.html', {
+        'conversation': conversation
+    })
