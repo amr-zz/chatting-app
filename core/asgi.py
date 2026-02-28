874dc8e57f5efda1324bb95b6c0fc34780118dfe
Amr Mohamed <amrmohm629@gmail.com>
2026-02-28 07:20:05 +0200

    (feat): add jwt middleware for websocket connections

--- core/asgi.py
+++ core/asgi.py
@@ -10,7 +10,16 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from chat.routing import websocket_urlpatterns
+from channels.routing import ProtocolTypeRouter, URLRouter
+from core.chat_config.jwt_middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': JWTAuthMiddleware(URLRouter(
+        websocket_urlpatterns,
+    )),
+})
+
